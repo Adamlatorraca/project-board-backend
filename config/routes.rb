ce468f4015746039a2af65aea307c071228eb0ea
Adamlatorraca <adam.latorraca@gmail.com>
2020-09-25 11:55:17 -0400

    add namespaced routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  resources :projects
-  resources :accounts
+
+  #fetch localhost:3000/api/v1/whatever
+
+  namespace :api do
+    namespace :v1 do
+      resources :projects
+      resources :comments
+      resources :accounts
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
