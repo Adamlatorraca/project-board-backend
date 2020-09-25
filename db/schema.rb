7b806316262d347b2bd0b91b61b4f58969a682cf
Adamlatorraca <adam.latorraca@gmail.com>
2020-09-25 11:55:37 -0400

    add comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_22_224905) do
+ActiveRecord::Schema.define(version: 2020_09_25_152530) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.integer "account_id"
+    t.string "project_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
